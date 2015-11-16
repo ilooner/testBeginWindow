95bc3a84071e1dd813f3b28faf96f745c29005c6
Timothy Farkas <tim@datatorrent.com>
2015-11-15 19:49:32 -0800

    Decreased state size.

--- src/main/java/com/datatorrent/RandomNumberConsumer.java
+++ src/main/java/com/datatorrent/RandomNumberConsumer.java
@@ -55,7 +55,7 @@ public class RandomNumberConsumer implements Operator
     if(state == null) {
       state = Lists.newArrayList();
 
-      for(long counter = 0; counter < 30000000; counter++) {
+      for(long counter = 0; counter < 20000000; counter++) {
         state.add(counter);
       }
     }
